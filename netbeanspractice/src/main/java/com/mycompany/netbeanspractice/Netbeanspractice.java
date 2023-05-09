a4f912a83696fd9252cf883c7f6ecd2e89ea45b8
Lien Nguyen <liennguyen@Liens-MacBook-Pro.local>
2023-05-09 14:10:40 -0400

    added first netbeans code

---
+++ netbeanspractice/src/main/java/com/mycompany/netbeanspractice/Netbeanspractice.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.netbeanspractice;
+
+/**
+ *
+ * @author liennguyen
+ */
+public class Netbeanspractice {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! It's Lien. Excited to learn Java.");
+    }
+}
