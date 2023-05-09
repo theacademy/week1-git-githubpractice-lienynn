bea7763c805f19af1befac6f2310b6fbba83537d
Lien Nguyen <liennguyen@Liens-MacBook-Pro.local>
2023-05-09 16:04:37 -0400

    fixed typo errors

--- intellijpractice/src/Lien.java
+++ intellijpractice/src/Lien.java
@@ -1,5 +1,5 @@
 public class Lien {
     public static void main(String[] args) {
-        System.out.println("I'm Lien! /n This is my frist code using Intellij");
+        System.out.println("I'm Lien! This is my first code using Intellij");
     }
 }
