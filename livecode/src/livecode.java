050f5c9f4dd39c053d873c62072d4219f2edbd1d
Lien Nguyen <liennguyen@Liens-MacBook-Pro.local>
2023-05-09 16:26:14 -0400

    livecode

---
+++ livecode/src/livecode.java
@@ -0,0 +1,10 @@
+public class livecode {
+    public static void main(String[] args) {
+        // Press Opt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+
+
+        }
+    }
+
